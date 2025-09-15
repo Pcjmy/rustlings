0842ded6e987789cb5f268aa876faa453afa1bd2
Pcjmy <1716045191@qq.com>
2025-09-16 00:00:45 +0800

    feat: quiz

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -14,6 +14,19 @@
 
 fn main() {
     // You can optionally experiment here.
+    let quantity = 41;
+    let price = calculate_price_of_apples(quantity);
+    println!("The price of {} apples is {} rustbucks.", quantity, price);
+}
+
+// TODO: Write a function that calculates the price of an order of apples given
+// the quantity bought.
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
 }
 
 // Don't change the tests!
